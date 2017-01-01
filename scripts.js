630bdde1945452bb984bd2642a0ffe43adfe38a4
erosspl <eros.z@poczta.fm>
2017-01-01 15:19:47 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,8 +8,7 @@ $(function(){
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-   		
+		carouselList.css({marginLeft:0});   		
   	};
   	
 	 function changeSlide(){
