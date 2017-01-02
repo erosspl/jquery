c312509bd9abbf07a60cbeb75b2d19e67bb32e10
Pawel Glowacki <eros.z@poczta.fm>
2017-01-02 19:27:29 +0100

    carousel 1st enchancement

--- scripts.js
+++ scripts.js
@@ -1,19 +1,40 @@
 $(function(){
-	console.log("DOM loaded!");
+	console.log('DOM loaded!');
 
-	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 3000);
+	var carouselList = $('#carousel ul');
+	var interval = setInterval(changeSlide, 3000);
 
-	 function moveFirstSlide(){
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+	var leftButton = document.getElementById('left');
+	leftButton.addEventListener('click', changeSlide);
+
+	var rightButton = document.getElementById('right');
+	rightButton.addEventListener('click', moveRightSlide);
+
+	$('#box').hover(function() {
+    	window.clearInterval(interval);    
+		}, 
+	function() {
+    	interval = window.setInterval(function(){changeSlide()},3000);
+	});
+
+	function moveLeftSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-   		
+		carouselList.css({ marginLeft: 0 });
+   	};
+
+  	function moveRightSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem);
+		carouselList.css({ marginLeft: -600 });
+		carouselList.animate({ 'marginLeft': 0 }, 1000);
+		
   	};
   	
-	 function changeSlide(){
-	 	carouselList.animate({'marginLeft':-600}, 1000, moveFirstSlide);
+	 function changeSlide() {
+	 	carouselList.animate({'marginLeft':-600}, 1000, moveLeftSlide);
 	 };
   
 
